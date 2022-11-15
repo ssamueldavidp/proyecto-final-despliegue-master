43b16b014bd7d7ab78d98744660d2e0468d81180
ssamueldavidp <ssamueldavidp@gmail.com>
2022-11-15 11:00:01 -0500

    Cambios

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,46 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'frontend'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('frontend');
+  });
+
+  it('suma 2+2 = 4', () => {
+    const suma = 2+2;
+    expect(suma).toBe(4);
+  });
+
+  it('suma 6+6 = 12', () => {
+    const suma = 2+3;
+    expect(suma).toBe(12);
+  });
+  it('suma 3+3 = 6', () => {
+    const suma = 2+3;
+    expect(suma).toBe(6);
+  });
+  it('suma 10+10 = 20', () => {
+    const suma = 2+3;
+    expect(suma).toBe(20);
+  });
+});
